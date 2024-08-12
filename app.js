171c190135dd8b7036ca23e6fe2f2363965e7593
Rajakatari <112773012+Rajakatari@users.noreply.github.com>
2024-08-12 18:59:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(cors());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "DELETE"],
+    origin: "https://trading-orders-by-raja.netlify.app",
+    methods: ["GET", "POST","POST", "DELETE"],
   })
 );
 
